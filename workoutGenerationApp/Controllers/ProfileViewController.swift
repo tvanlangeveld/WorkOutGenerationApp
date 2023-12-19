4739f213cdf849e3813d1a3cc672a7e0f5243c11
Tanner Vanlangeveld <t.vanlangeveld@outlook.com>
2023-12-18 17:59:05 -0700

    Added Delete button feature for tableview on profile VC.

--- workoutGenerationApp/Controllers/ProfileViewController.swift
+++ workoutGenerationApp/Controllers/ProfileViewController.swift
@@ -14,7 +14,8 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
+        tableView.isEditing = true
+
         tableView.register(UINib(nibName: "ProfileViewCell", bundle: nil), forCellReuseIdentifier: "profileID")
 
     }
@@ -103,6 +104,12 @@ extension ProfileViewController: UITableViewDataSource{
     }
     
     
-    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            WorkOuts.shared.exercises.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+    }
     
 }
